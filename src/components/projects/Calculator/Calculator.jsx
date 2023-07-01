85e0e685b0723cf484cb53b700e078efc1727ed0
ansachuk <ansachuk@icloud.com>
2023-07-01 23:46:02 +0300

    start calculator

---
+++ src/components/projects/Calculator/Calculator.jsx
@@ -0,0 +1,42 @@
+import { numbers, mainOptions, secondlyOptions } from "data/calculatorData";
+import css from "./Calculator.module.scss";
+import { useState } from "react";
+
+export default function Calculator() {
+	const [output, setOutput] = useState(0);
+	return (
+		<div className={css.calculator}>
+			<div className={css.wrapper__output}>
+				<p className={css.output}>{output}</p>
+			</div>
+			<div className={css.calc}>
+				<div className={css.calc__mainWrapper}>
+					<div className={css.calc__numbers}>
+						{numbers.map(num => (
+							<button key={num} data-value={num}>
+								{num}
+							</button>
+						))}
+					</div>
+					<div className={css.calc__mainOption}>
+						{mainOptions.map(opt => (
+							<button key={opt} data-value={opt}>
+								{opt}
+							</button>
+						))}
+					</div>
+				</div>
+				<div className={css.calc__bottomOption}>
+					{secondlyOptions.map(opt => (
+						<button key={opt} data-value={opt}>
+							{opt}
+						</button>
+					))}
+				</div>
+			</div>
+			<button type="button" className={css.clear}>
+				CLEAR
+			</button>
+		</div>
+	);
+}
--- src/utils/checkCurrentProject.js
+++ src/utils/checkCurrentProject.js
@@ -1,8 +1,8 @@
 import { Navigate } from "react-router-dom";
 
-import Calculator from "components/Calculator/Calculator";
-import ExpandingCards from "components/ExpandingCards/ExpandingCards";
-import ProgressSteps from "components/ProgressSteps/ProgressSteps";
+import Calculator from "components/projects/Calculator/Calculator";
+import ExpandingCards from "components/projects/ExpandingCards/ExpandingCards";
+import ProgressSteps from "components/projects/ProgressSteps/ProgressSteps";
 
 export default function checkCurrentProject(projectName) {
 	switch (projectName) {
